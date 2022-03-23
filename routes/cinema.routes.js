665640f36c79e426426ea02c995b04e4d5676733
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-03-23 17:35:11 +0100

    basic cinema view page

--- routes/cinema.routes.js
+++ routes/cinema.routes.js
@@ -8,4 +8,16 @@ router.get('/', async (req, res, next) => {
   res.render('cinema', { cinemas })
 })
 
+/* GET single cinema view page */
+router.get('/:id', async (req, res, next) => {
+  try {
+    const cinema = await Cinema.findById(req.params.id)
+    console.log(cinema)
+
+    res.render('cinema/view', { cinema })
+  } catch (error) {
+    next(error)
+  }
+})
+
 module.exports = router
