a2ad23cf64aa6b059db416fa2135bd11941765f2
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-03-26 23:29:10 +0100

    error handling date handlebars helpers

--- config/handlebars.js
+++ config/handlebars.js
@@ -2,19 +2,26 @@ const hbs = require('hbs')
 
 hbs.registerPartials('views/partials')
 
-hbs.registerHelper('time', (datetime) => {
-  let { format } = Intl.DateTimeFormat('fr-FR', {
-    hour: 'numeric',
-    minute: 'numeric',
-  })
-  return format(datetime)
-})
+const timeFormat = {
+  hour: 'numeric',
+  minute: 'numeric',
+}
 
-hbs.registerHelper('date', (datetime) => {
-  let { format } = Intl.DateTimeFormat('fr-FR', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  })
-  return format(datetime)
-})
+const dateFormat = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+}
+
+const formatDate = (datetime, targetFormat) => {
+  try {
+    let { format } = Intl.DateTimeFormat('fr-FR', targetFormat)
+    return format(datetime)
+  } catch (error) {
+    console.error('uhohohohoh', datetime)
+    return datetime
+  }
+}
+
+hbs.registerHelper('time', (datetime) => formatDate(datetime, timeFormat))
+hbs.registerHelper('date', (datetime) => formatDate(datetime, dateFormat))
