fab5a66b56573d38da1333c5400b707e83623eaa
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-03-26 01:13:07 +0100

    favourite cinema add/remove

--- routes/cinema.routes.js
+++ routes/cinema.routes.js
@@ -1,5 +1,6 @@
 const { getShowtimes } = require('../api/allocine')
 const Cinema = require('../models/Cinema.model')
+const FavouriteCinema = require('../models/FavouriteCinema.model')
 
 const router = require('express').Router()
 
@@ -12,8 +13,17 @@ router.get('/', async (req, res, next) => {
 /* GET single cinema view page */
 router.get('/:id', async (req, res, next) => {
   try {
-    const cinema = await Cinema.findById(req.params.id)
+    let cinema = await Cinema.findById(req.params.id)
     const showtimes = await getShowtimes(cinema.allocine_id)
+
+    if (req.session.user) {
+      const liked =
+        (await FavouriteCinema.count({
+          cinema: cinema._id,
+          user: req.session.user._id,
+        })) > 0
+      cinema = { liked, ...cinema._doc }
+    }
 
     res.render('cinema/view', { cinema, showtimes })
   } catch (error) {
