5e0f748b72a7ecaea5331cbd03537bc4afcd8fb5
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-03-28 16:06:10 +0200

    viewing the mongo_uri in logs when the database connection fails

--- db/index.js
+++ db/index.js
@@ -13,7 +13,7 @@ const connection = mongoose
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
   .catch((err) => {
-    console.error('Error connecting to mongo: ', err)
+    console.error(`Error connecting to mongo: ${MONGO_URI}.`, err)
   })
 
 module.exports = connection
