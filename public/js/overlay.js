40a6ea0aacfe63b5488085942c3a132a5a497006
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-03-30 12:59:51 +0200

    removing console logs

--- public/js/overlay.js
+++ public/js/overlay.js
@@ -2,18 +2,13 @@ const containerClass = 'overlay-container'
 const showOverlayClass = 'show-overlay'
 
 const toggleShow = (container) => {
-  console.log('TOGGLING SHOW', container)
   container.classList.toggle(showOverlayClass)
 }
 
 const setup = () => {
   const containers = document.querySelectorAll('.' + containerClass)
   containers.forEach((container) =>
-    container.addEventListener('touchstart', () => toggleShow(container))
-  )
-
-  document.addEventListener('touchstart', ({ target }) =>
-    console.log('touch', target)
+    container.addEventListener('touchend', () => toggleShow(container))
   )
 }
 
