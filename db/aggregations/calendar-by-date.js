9fc9180926b85defe89a90920822e0e9fd39baa5
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-03-31 18:12:28 +0200

    calendar-by-date.js missed in pull request #23

---
+++ db/aggregations/calendar-by-date.js
@@ -0,0 +1,32 @@
+const Calendar = require('../../models/Calendar.model')
+const {
+  match,
+  sortByStartTime,
+  groupByDate,
+  projectToShowtime,
+  populateMovie,
+  populateShowtime,
+  populateCinema,
+  unwindMovie,
+  unwindShowtime,
+  unwindCinema,
+} = require('./steps')
+
+const getCalendarForUserGroupByDate = async (userId) => {
+  const calendar = await Calendar.aggregate([
+    match(userId, 'user'),
+    populateShowtime,
+    unwindShowtime,
+    projectToShowtime,
+    populateMovie,
+    unwindMovie,
+    populateCinema,
+    unwindCinema,
+    groupByDate,
+  ])
+  return calendar
+}
+
+module.exports = {
+  getCalendarForUserGroupByDate,
+}
