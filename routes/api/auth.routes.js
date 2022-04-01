f0f0b4b3043e43ec607e68eced3b88d70846347b
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-01 11:26:37 +0200

    multiple error messages support

--- routes/api/auth.routes.js
+++ routes/api/auth.routes.js
@@ -5,43 +5,56 @@ const mongoose = require('mongoose')
 const { ALGORITHM } = require('../../config/jwt')
 const { isAuthenticated } = require('../../middleware/jwt.middleware')
 const User = require('../../models/User.model')
-const { createUser } = require('../helpers/user')
+const { createUser, validateUserInputs } = require('../helpers/user')
+
+const sendBadRequestResponse = (res, ...errorMessages) => {
+  return res.status(400).json({
+    errorMessages,
+    // backwards compatibility
+    errorMessage: errorMessages.join(' '),
+  })
+}
 
 router.post('/signup', async (req, res, _next) => {
-  const { username, password, profilePictureUrl } = req.body
+  const { username, password, profilePictureUrl, email } = req.body
 
-  if (!username) {
-    return res.status(400).json({
-      errorMessage: 'Please provide your username.',
-    })
+  const validationErrors = Array.from(
+    ...validateUserInputs({ username, email, password })
+  )
+  if (validationErrors.length > 0) {
+    return sendBadRequestResponse(res, ...validationErrors)
   }
 
-  if (password.length < 8) {
-    return res.status(400).json({
-      errorMessage: 'Your password needs to be at least 8 characters long.',
-    })
-  }
-
-  const foundUser = await User.findOne({ username })
+  const foundUser = await User.findOne({ $or: [{ username }, { email }] })
   if (foundUser) {
-    return res.status(400).json({ errorMessage: 'Username already taken.' })
+    const errorMessages = []
+    if (foundUser.email === email) {
+      errorMessages.push(`"${email}" already has an account.`)
+    }
+    if (foundUser.username === username) {
+      errorMessages.push(`Username "${username}" is already taken.`)
+    }
+
+    return sendBadRequestResponse(res, ...errorMessages)
   }
+
   try {
     const user = await createUser({
       username,
       profilePictureUrl,
       password,
+      email,
     })
     res.status(201).json(user)
   } catch (error) {
     if (error instanceof mongoose.Error.ValidationError) {
-      return res.status(400).json({ errorMessage: error.message })
+      return sendBadRequestResponse(res, error.message)
     }
     if (error.code === 11000) {
-      return res.status(400).json({
-        errorMessage:
-          'Username need to be unique. The username you chose is already in use.',
-      })
+      return sendBadRequestResponse(
+        res,
+        'Username needs to be unique. The username you chose is already in use.'
+      )
     }
     return res.status(500).json({ errorMessage: error.message })
   }
@@ -51,15 +64,14 @@ router.post('/login', async (req, res, _next) => {
   const { username, password } = req.body
 
   if (!username) {
-    return res.status(400).json({
-      errorMessage: 'Please provide your username.',
-    })
+    return sendBadRequestResponse(res, 'Please provide your username.')
   }
 
   if (password.length < 8) {
-    return res.status(400).json({
-      errorMessage: 'Your password needs to be at least 8 characters long.',
-    })
+    return sendBadRequestResponse(
+      res,
+      'Your password needs to be at least 8 characters long.'
+    )
   }
   try {
     const foundUser = await User.findOne({ username }).select(
@@ -67,18 +79,14 @@ router.post('/login', async (req, res, _next) => {
     )
 
     if (!foundUser) {
-      return res.status(400).json({
-        errorMessage: 'Wrong credentials.',
-      })
+      return sendBadRequestResponse(res, 'Wrong credentials.')
     }
 
     const { password: foundPassword, ...payload } = foundUser._doc
     const isCorrectPassword = await bcrypt.compare(password, foundPassword)
 
     if (!isCorrectPassword) {
-      return res.status(400).json({
-        errorMessage: 'Wrong credentials.',
-      })
+      return sendBadRequestResponse(res, 'Wrong credentials.')
     }
 
     const authToken = jsonwebtoken.sign(payload, process.env.TOKEN_SECRET, {
