321c2dc9253be48cd5f29153c2759ff129a1d345
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-01 11:26:37 +0200

    multiple error messages support

--- routes/api/auth.routes.js
+++ routes/api/auth.routes.js
@@ -5,43 +5,56 @@ const mongoose = require('mongoose')
 const { ALGORITHM } = require('../../config/jwt')
 const { isAuthenticated } = require('../../middleware/jwt.middleware')
 const User = require('../../models/User.model')
-const { createUser } = require('../helpers/user')
+const { createUser, validateUserInputs } = require('../helpers/user')
+
+const sendBadRequestResponse = (res, ...errorMessages) => {
+  return res.status(400).json({
+    errorMessages,
+    // backwards compatibility
+    errorMessage: errorMessages.join(' '),
+  })
+}
 
 router.post('/signup', async (req, res) => {
-  const { username, password, profilePictureUrl } = req.body
+  const { username, password, profilePictureUrl, email } = req.body
 
-  if (!username) {
-    return res.status(400).json({
-      errorMessage: 'Please provide your username.',
-    })
+  const validationErrors = Array.from(
+    ...validateUserInputs({ username, email, password })
+  )
+  if (validationErrors.length > 0) {
+    return sendBadRequestResponse(res, ...validationErrors)
   }
 
-  if (password.length < 8) {
-    return res.status(400).json({
-      errorMessage: 'Your password needs to be at least 8 characters long.',
-    })
-  }
-
-  const foundUser = await User.findOne({ username })
+  const foundUser = await User.findOne({ $or: [{ username }, { email }] })
   if (foundUser) {
-    return res.status(400).json({ errorMessage: 'Username already taken.' })
+    const errorMessages = []
+    if (foundUser.email === email) {
+      errorMessages.push(`"${email}" already has an account.`)
+    }
+    if (foundUser.username === username) {
+      errorMessages.push(`Username "${username}" is already taken.`)
+    }
+
+    return sendBadRequestResponse(res, ...errorMessages)
   }
+
   try {
     const user = await createUser({
       username,
       profilePictureUrl,
       password,
+      email,
     })
     res.status(201).json(user)
   } catch (error) {
     if (error instanceof mongoose.Error.ValidationError) {
-      return res.status(400).json({ errorMessage: error.message })
+      return sendBadRequestResponse(res, error.message)
     }
     if (error.code === 11000) {
-      return res.status(400).json({
-        errorMessage:
-          'Username need to be unique. The username you chose is already in use.',
-      })
+      return sendBadRequestResponse(
+        res,
+        'Username needs to be unique. The username you chose is already in use.'
+      )
     }
     return res.status(500).json({ errorMessage: error.message })
   }
@@ -51,31 +64,26 @@ router.post('/login', async (req, res, next) => {
   const { username, password } = req.body
 
   if (!username) {
-    return res.status(400).json({
-      errorMessage: 'Please provide your username.',
-    })
+    return sendBadRequestResponse(res, 'Please provide your username.')
   }
 
   if (password.length < 8) {
-    return res.status(400).json({
-      errorMessage: 'Your password needs to be at least 8 characters long.',
-    })
+    return sendBadRequestResponse(
+      res,
+      'Your password needs to be at least 8 characters long.'
+    )
   }
   try {
     const user = await User.findOne({ username })
 
     if (!user) {
-      return res.status(400).json({
-        errorMessage: 'Wrong credentials.',
-      })
+      return sendBadRequestResponse(res, 'Wrong credentials.')
     }
 
     const isCorrectPassword = await bcrypt.compare(password, user.password)
 
     if (!isCorrectPassword) {
-      return res.status(400).json({
-        errorMessage: 'Wrong credentials.',
-      })
+      return sendBadRequestResponse(res, 'Wrong credentials.')
     }
 
     const payload = { username, profilePictureUrl: user.profilePictureUrl }
--- routes/helpers/user.js
+++ routes/helpers/user.js
@@ -14,6 +14,29 @@ const createUser = async ({ username, profilePictureUrl, password }) => {
   return user
 }
 
+const emailRegex =
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i
+
+const validateUserInputs = function* ({ username, email, password }) {
+  if (!username || typeof username !== 'string' || username.length < 4) {
+    yield 'Username must be a string of at least 4 characters.'
+  }
+
+  if (
+    !email ||
+    typeof email !== 'string' ||
+    email.length < 5 ||
+    !emailRegex.test(email)
+  ) {
+    yield 'Invalid email format.'
+  }
+
+  if (password.length < 8) {
+    yield 'Password must be at least 8 characters long.'
+  }
+}
+
 module.exports = {
   createUser,
+  validateUserInputs,
 }
