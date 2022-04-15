17277731affb9a5e81f583533654abe5b17899fd
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-15 11:08:07 +0200

    showtimes for date API route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const authRouter = require('./auth.routes')
 const moviesRouter = require('./movies.routes')
+const showtimesRouter = require('./showtimes.routes')
 
 router.use('/auth', authRouter)
 router.use('/movies', moviesRouter)
+router.use('/showtimes', showtimesRouter)
 
 module.exports = router
