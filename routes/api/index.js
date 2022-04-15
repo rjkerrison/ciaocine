afee20980319d7e067fa7f31cd177de158436e83
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-15 11:25:22 +0200

    404 for API

--- routes/api/index.js
+++ routes/api/index.js
@@ -7,6 +7,13 @@ router.use('/auth', authRouter)
 router.use('/movies', moviesRouter)
 router.use('/showtimes', showtimesRouter)
 
+router.use((req, res, next) => {
+  // 404 for API
+  return res
+    .status(404)
+    .json({ errorMessage: `Cannot ${req.method} ${req.url}` })
+})
+
 router.use((error, req, res, next) => {
   // Error handling for API
   return res.status(500).json({ errorMessage: error.message })
