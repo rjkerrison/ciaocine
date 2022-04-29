87e669b64b56275e98c1207dccdfbc169ad10e48
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-04-29 13:52:32 +0100

    deleting all views routes

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,12 @@
-// 👇 Start handling routes here
 const index = require('./index.routes')
-const authRoutes = require('./auth.routes')
-const cinemaRoutes = require('./cinema.routes')
-const calendarRoutes = require('./calendar.routes')
-const favouriteRoutes = require('./favourite.routes')
-const moviesRoutes = require('./movies.routes')
 const apiRouter = require('./api')
 
 const registerRoutes = (app) => {
-  app.use('/', index)
-  app.use('/auth', authRoutes)
-  app.use('/cinema', cinemaRoutes)
-  app.use('/calendar', calendarRoutes)
-  app.use('/favourite', favouriteRoutes)
-  app.use('/movies', moviesRoutes)
+  // Two base routes for compatibility
+  app.use('/', apiRouter)
   app.use('/api', apiRouter)
+  // Contains a redirect to the real homepage
+  app.use('/', index)
 }
 
 module.exports = registerRoutes
