9ec4aa6f3e3a0aa0e50f124af8392bdc9f08c56c
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-06-14 13:07:48 +0200

    rebuild indexes late, not early

--- bin/cinemas/slugs.js
+++ bin/cinemas/slugs.js
@@ -1,8 +1,6 @@
 const Cinema = require('../../models/Cinema.model')
 
 const addSlugsToCinemas = async () => {
-  // Rebuild indexes
-  await Cinema.init()
   // Find
   const cinemas = await Cinema.find({ slug: { $exists: 0 } })
   console.log('Found %s cinemas without slugs.', cinemas.length)
@@ -11,6 +9,9 @@ const addSlugsToCinemas = async () => {
     const updatedCinema = await cinema.save()
     console.log('Updated cinema:', updatedCinema)
   }
+
+  // Rebuild indexes
+  await Cinema.init()
 
   console.log(`Updated ${cinemas.length} cinemas.`)
 }
