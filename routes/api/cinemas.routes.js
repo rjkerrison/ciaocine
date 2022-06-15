90d46c6df5a5eacac783cc263b3b6483322bbe99
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-06-15 13:42:39 +0200

    adding /cinemas/nearby search

--- routes/api/cinemas.routes.js
+++ routes/api/cinemas.routes.js
@@ -2,11 +2,36 @@ const router = require('express').Router()
 const { getMovies } = require('../helpers/movies')
 const { getDateParams } = require('../helpers/dates')
 const Cinema = require('../../models/Cinema.model')
+const { getLocation } = require('../../api/geocode')
 
 /* GET /api/cinemas */
 router.get('/', async (req, res, next) => {
   let cinemas = await Cinema.find()
   res.status(200).json({ cinemas })
+})
+
+/* GET /api/cinemas */
+router.get('/nearby', async (req, res, next) => {
+  try {
+    const q = req.query.q
+    if (!q) {
+      res.status(401).json({ message: `Please provide a query parameter 'q'.` })
+      return
+    }
+
+    const location = await getLocation(q)
+    if (!location) {
+      res.status(400).json({ message: `Unknown location: ${q}` })
+      return
+    }
+
+    const cinemas = await Cinema.find({
+      geolocation: { $near: { $geometry: location } },
+    })
+    res.status(200).json({ cinemas })
+  } catch (error) {
+    next(error)
+  }
 })
 
 /* GET /api/cinemas/:cinemaId */
