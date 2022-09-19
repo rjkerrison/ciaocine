3f49d00d83a7b623dd072aef66f4612ebbdbf645
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-09-19 17:46:08 +0200

    DELETE routes for relationships

--- routes/api/movie-relationships.routes.js
+++ routes/api/movie-relationships.routes.js
@@ -27,6 +27,15 @@ const upsertRelationship = async (relationshipModel, req, res) => {
   })
 }
 
+const removeRelationship = async (relationshipModel, req, res) => {
+  await relationshipModel.findOneAndDelete({
+    user: req.user.id,
+    movie: req.movie.id,
+  })
+
+  res.sendStatus(204)
+}
+
 router.post('/watch', (req, res, _next) => {
   upsertRelationship(Watch, req, res)
 })
@@ -37,6 +46,18 @@ router.post('/dismiss', async (req, res, _next) => {
 
 router.post('/want', async (req, res, _next) => {
   upsertRelationship(Want, req, res)
+})
+
+router.delete('/watch', async (req, res, _next) => {
+  removeRelationship(Watch, req, res)
+})
+
+router.delete('/dismiss', async (req, res, _next) => {
+  removeRelationship(Dismiss, req, res)
+})
+
+router.delete('/want', async (req, res, _next) => {
+  removeRelationship(Want, req, res)
 })
 
 module.exports = router
